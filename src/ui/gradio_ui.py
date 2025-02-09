ba47b8ab692cbf1ffc8d90a434f7d5e001adf45e
Flo Kempenich <flo@kempenich.dev>
2025-02-09 22:36:44 +0000

    Experiment w/ Gradio

---
+++ src/ui/gradio_ui.py
@@ -0,0 +1,43 @@
+import gradio as gr
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.constants import START, END
+from langgraph.graph import MessagesState, StateGraph
+
+from src.common.utils import get_model
+
+model = get_model(local=False)
+
+
+def invoke_chatbot(state: MessagesState):
+    resp = model.invoke(state["messages"])
+    return {"messages": resp}
+
+
+graph_builder = StateGraph(state_schema=MessagesState)
+
+graph_builder.add_node("chatbot", invoke_chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+
+def random_response(message, _history):
+    config = {
+        "configurable": {
+            "thread_id": "abc123"
+        }
+    }
+    resp = graph.invoke({"messages": [HumanMessage(message)]}, config)
+    return resp["messages"][-1].content
+
+
+demo = gr.ChatInterface(
+    fn=random_response,
+    type="messages"
+)
+
+if __name__ == '__main__':
+    demo.launch()
